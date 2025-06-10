a12ac002028979c2798e7b788d1f5d3d101e714e
MissTee11 <zizhtarie@gmail.com>
2025-06-10 17:44:41 +0300

    Fetched room data and displayed it to front end

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,6 +1,6 @@
 import DataTable from "react-data-table-component";
 import { Link } from 'react-router-dom';
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import { RiCalendarScheduleFill } from "react-icons/ri";
 import Sidebar from '../components/Sidebar';
 import {  useNavigate } from 'react-router-dom';
@@ -9,13 +9,31 @@ import { customStyles } from "../utilities/dataTableCustomStyles";
 import { myCustomTheme } from "../utilities/dataTableTheme";
 import { getRooms } from "../api";
 function Rooms(){
+
     const [messageText, setMessageText] =useState("");
     const navigate = useNavigate();
+    const [rooms, setRooms]= useState([]);
+
+    useEffect(()=>{
+     const fetchRooms = async () => {
+                try {
+                  const res = await getRooms();
+                  console.log(res.data);
+                  setRooms(res.data);
+                   
+                } catch (err) {
+                  console.error("Error fetching specialty:", err);
+                }
+              };
+              fetchRooms();
+            }, []);
+    
+    
 
     const handleMakeAppointment= async (availability_status, event)=>{
       if (event) event.preventDefault();
-      
-        if(availability_status !=="available"){
+
+        if(availability_status !=="Available"){
             setMessageText("ROOM IN USE");
 
             setTimeout(() => {
@@ -24,7 +42,7 @@ function Rooms(){
 
         }
         else{
-            navigate('/Appointments');
+            navigate('/AddAdmissions');
         }
 
     }
@@ -35,20 +53,18 @@ function Rooms(){
           selector:row =>row.room_number
         },
         {
-          name: 'Availability Status',
-          selector:row =>row.availability_status
-        },
-        {
           name: 'Room Type',
           selector:row =>row.room_type
+        },
+         {
+          name: 'Availability Status',
+          selector:row =>row.availability_status
         },
         {
           name: 'Actions',
           cell: row => (
             <div >
-              <Link to='/AddAppointment'>
-              <button className="UpdateBtn" onClick={(e)=>handleMakeAppointment(row.availability_status,e)}><RiCalendarScheduleFill/>Make Appointment</button>
-              </Link>
+              <button className="UpdateBtn" onClick={(e)=>handleMakeAppointment(row.availability_status,e)}><RiCalendarScheduleFill/>Admit Patient</button>
              
          </div>
           ),
@@ -62,7 +78,7 @@ function Rooms(){
              <div className="MainContent">          
             <DataTable
             columns={columns}
-            data={columns}
+            data={rooms}
             customStyles={customStyles}
             theme="myCustomTheme"
             responsive>
