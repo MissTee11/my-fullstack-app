ae19973ce99c9832c3c46eb0ab699b9f4cad9801
MissTee11 <zizhtarie@gmail.com>
2025-07-14 16:51:17 +0300

    Staff slice

---
+++ src/redux/slices/staffSlice.js
@@ -0,0 +1,23 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { getStaff } from '../../api';
+
+export const fetchStaff = createAsyncThunk('staff/fetchStaff', async ()=>{
+    const data = await getStaff();
+    return data;
+});
+
+const staffSlice= createSlice({
+    name:'staff',
+    initialState:{
+        list: [],
+        total: 0,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchStaff.fulfilled, (state, action)=>{
+            state.list = action.payload;
+            state.total = action.payload.length;
+        });
+    },
+});
+export default fetchStaff.reducer;
