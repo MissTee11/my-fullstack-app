ade993ba0317e9aef57bf5ca03da3dfda6c7128b
MissTee11 <zizhtarie@gmail.com>
2025-07-14 16:32:32 +0300

    installed react redux and set up store.js

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,19 @@
 import Sidebar from "../components/Sidebar";
+
 function Dashboard(){
     return(
 
         <div>
             <Sidebar/>
-            <p>Welcome </p>
+            <div className='Cards'>
+
+                <div className='patientCard'>
+                    <p>Total Patients</p>
+                    
+                </div>
+
+            </div>
+          
         </div>
     )
 
