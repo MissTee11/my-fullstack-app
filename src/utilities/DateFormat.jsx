e1b34088c08ba52bb19f088eddbc4f19d3cf3eb7
MissTee11 <zizhtarie@gmail.com>
2025-07-17 14:55:00 +0300

    Date utility code review

--- src/utilities/DateFormat.jsx
+++ src/utilities/DateFormat.jsx
@@ -1,7 +1,9 @@
-
+//format dates for consistency in backend and frontend
+//Postgresql stores dates with time in UTC 
+//React inputs of type date expect YYYY-MM-DD no time
 export const formatDateInput = (date) => {
   if (!date) return "";
-  const d = new Date(date);
+  const d = new Date(date);//converts to local time
   const year = d.getFullYear();
   const month = String(d.getMonth() + 1).padStart(2, '0'); 
   const day = String(d.getDate()).padStart(2, '0');
