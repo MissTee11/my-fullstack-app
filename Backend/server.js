b4b197bedff570ac5354a4424a4c82638961b083
MissTee11 <zizhtarie@gmail.com>
2025-07-22 17:27:36 +0300

    Added all possible urls

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'https://hospital-management-system-two-orpin.vercel.app',
+  'https://hospital-management-system-git-main-misstee11s-projects.vercel.app',
+  'https://hospital-management-system-l1d05pjf7-misstee11s-projects.vercel.app',
 ];
 
 app.use(cors({
