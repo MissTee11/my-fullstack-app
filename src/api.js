2c2110a651f5336c83629cf1ed9aae749cc1f49c
MissTee11 <zizhtarie@gmail.com>
2025-07-22 16:06:45 +0300

    API CHANGES

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
 /*const API_BASE_URL ="http://localhost:5000/api";*/
 const API_BASE_URL = process.env.REACT_APP_BACKEND_URL + "/api";
 
-console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
+
 
 
 /*USER AUTHENTIATION*/
