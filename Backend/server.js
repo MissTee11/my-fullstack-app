67f15c85458e9a2e756e21fc81382a08b05e9759
MissTee11 <zizhtarie@gmail.com>
2025-07-22 13:27:07 +0300

    Added frontend url

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const pool = require('./db');
 const app=express();
 const PORT = process.env.PORT || 5000;
 
-/*const allowedOrigins = ['https://your-frontend.vercel.app'];
+const allowedOrigins = ['https://hospital-management-system-5ploufdsw-misstee11s-projects.vercel.app'];
 
 app.use(cors({
   origin: function(origin, callback){
@@ -23,9 +23,9 @@ app.use(cors({
     return callback(null, true);
   },
   credentials: true, // allow cookies and auth headers like x-auth-token
-}));*/
+}));
 
-app.use(cors({ origin: true, credentials: true }));
+
 
 
 app.use(json());
