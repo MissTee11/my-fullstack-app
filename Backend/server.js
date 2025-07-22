d2c2c600beb28a23c8e82a4fc672da62301dcf0a
MissTee11 <zizhtarie@gmail.com>
2025-07-22 17:22:11 +0300

    CORS-backend not responding with requuired CORS headers fix

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,9 @@ app.use(cors({
     if (allowedOrigins.includes(origin)) return callback(null, true);
     return callback(new Error('CORS not allowed from this origin'), false);
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'x-auth-token']
 }));
 
 
