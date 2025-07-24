2a975e97de5bc345ae71bacf172af424d033ced5
MissTee11 <zizhtarie@gmail.com>
2025-07-24 11:35:21 +0300

    Correcting file path

--- Backend/server.js
+++ Backend/server.js
@@ -13,10 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Backend is live!' });
+});
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..','dist', 'index.html'));
 });
 
 app.use(json());
